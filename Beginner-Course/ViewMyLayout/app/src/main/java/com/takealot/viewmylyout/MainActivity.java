95d130ab698d5c748d68d92dfc72501f43510745
Kaylen Travis Pillay <kaylen.pillay@takealot.com>
2019-03-12 12:22:45 +0200

    Added more example project code

--- Beginner-Course/ViewMyLayout/app/src/main/java/com/takealot/viewmylyout/MainActivity.java
+++ Beginner-Course/ViewMyLayout/app/src/main/java/com/takealot/viewmylyout/MainActivity.java
@@ -2,6 +2,9 @@ package com.takealot.viewmylyout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myName = (TextView) findViewById(R.id.TV_name);
+    }
+
+    public void onMakeLongString(View view) {
+        TextView myName = findViewById(R.id.TV_name);
+        myName.setText(getResources().getString(R.string.long_string));
+        myName.setTextColor(getResources().getColor(R.color.takealotBlue));
     }
 }
