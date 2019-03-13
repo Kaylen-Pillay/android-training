81ad18421212adc36810db2ad8c85a4e19b28466
Kaylen Travis Pillay <kaylen.pillay@takealot.com>
2019-03-13 05:36:55 +0200

    Added all the files for the demo app

--- Beginner-Course/ViewMyLayout/app/src/main/java/com/takealot/viewmylyout/MainActivity.java
+++ Beginner-Course/ViewMyLayout/app/src/main/java/com/takealot/viewmylyout/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView myName = (TextView) findViewById(R.id.TV_name);
+        Button displayMessageButton = (Button) findViewById(R.id.display_message_button);
+        displayMessageButton.setOnClickListener(displayMessageButtonClickListener);
     }
 
-    public void onMakeLongString(View view) {
-        TextView myName = findViewById(R.id.TV_name);
-        myName.setText(getResources().getString(R.string.long_string));
-        myName.setTextColor(getResources().getColor(R.color.takealotBlue));
-    }
+    private View.OnClickListener displayMessageButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView displayMessageTextView = (TextView) findViewById(R.id.display_message);
+            Button displayMessageButton = (Button) view;
+
+            // Toggle the display message.
+            if (displayMessageTextView.isShown()) {
+                displayMessageButton.setText(getResources().getString(R.string.show));
+                displayMessageTextView.setVisibility(View.GONE);
+            } else {
+                displayMessageButton.setText(getResources().getString(R.string.hide));
+                displayMessageTextView.setVisibility(View.VISIBLE);
+
+                if (displayMessageTextView.getText().equals("")) {
+                    displayMessageTextView.setText(getResources().getString(R.string.long_string));
+                }
+            }
+        }
+    };
 }
